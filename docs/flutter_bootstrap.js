c795ea0c4477993e1b62722178c6108b2c884618
TetsuFe <tetsubeg1ner27@gmail.com>
2026-03-26 17:11:13 +0900

    Deploy Flutter Web

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"cafcda5721a78a7884db92f13c5e89f7643d52
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1868320860"
+    serviceWorkerVersion: "1807657020"
   }
 });
