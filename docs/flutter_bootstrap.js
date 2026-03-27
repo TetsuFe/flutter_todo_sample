9dff4796bbbe934060f7fe7f64db97125a9e86ac
TetsuFe <tetsubeg1ner27@gmail.com>
2026-03-27 16:29:59 +0900

    Deploy Flutter Web

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"cafcda5721a78a7884db92f13c5e89f7643d52
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1807657020"
+    serviceWorkerVersion: "1854986777"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ab235ca233796d9abd01e4e970c7e2bb",
+const RESOURCES = {"flutter_bootstrap.js": "6666d006e9826d0df41847dc6f2ef788",
 "version.json": "15297d0e02834fa8c2274be2b198a6a6",
 "index.html": "20c91a343ed0ff59ef439ea6cc5b3011",
 "/": "20c91a343ed0ff59ef439ea6cc5b3011",
-"main.dart.js": "8bb9df41082ccd2d5c929063b899d664",
+"main.dart.js": "7e45bea2fa38d40029464353c7a842a8",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
