55855fe6e6b726828b40c86efac6d2ddd46d5b1f
TetsuFe <tetsubeg1ner27@gmail.com>
2026-03-29 23:52:21 +0900

    refactor: main.dartとHomeScreenを複数ファイルに分割

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,31 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_sample/views/task/components/create_task_modal.dart';
 import 'package:flutter_todo_sample/views/task/components/task_list.dart';
-import 'package:flutter_todo_sample/views/theme/app_theme.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
-  runApp(ProviderScope(child: const MyApp(), retry: (_, _) => null));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Todo',
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'ホーム'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key, required this.title});
 
   final String title;
 
